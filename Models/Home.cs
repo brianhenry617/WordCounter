e123de14d3af5ac6528415fb690009939f754a44
Brian Henry <94brianhenry@gmail.com>
2018-05-06 20:21:53 -0700

    Update Models

--- Models/Home.cs
+++ Models/Home.cs
@@ -1,6 +1,35 @@
-using System.Collections.Generic;
-using System.IO;
 using System;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Home.Models
-/* code */
+namespace WordCounter.Models
+{
+  public class RepeatCounter
+  {
+    private static <Sentence> = new <Sentence> {};
+    private string _sentence;
+
+    public class RepeatCounter (string sentence)
+    {
+      _sentence = sentence;
+    }
+
+    public string GetSentence()
+    {
+      return _sentence;
+    }
+
+    public void SetSentence(newSentence){
+      _sentence = newSentence
+    }
+    public static <Sentence> GetAll()
+    {
+      return _instances
+    }
+    public void Save()
+    {
+      _instances.Add(this);
+    }
+  }
+}
